e638087423e4675209b9896b46f8ff0bd75d0232
ZoroDDragneel <53938369+ZoroDDragneel@users.noreply.github.com>
2020-11-23 20:02:58 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+    <Signup />
+  </div>
+</template>
+<script>
+import Signup from './components/Signup';
+
+
+export default {
+  name: 'App',
+  components: {
+    Signup
+  }
+}
+</script>
---
+++ src/components/Signup.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    EMAIL: <input type="text" v-model="email" /><br/>
+    PASSWORD: <input type="text" v-model="password" /><br/>
+    <button @click="signup">signup</button>
+  </div>
+</template>
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'Signup',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    signup(){
+      let newUser = {
+        email: this.email,
+        password: this.password
+      }
+      axios.post('http://localhost:5000/', newUser)
+    }
+  }
+}
+</script>
+
+
+ 
